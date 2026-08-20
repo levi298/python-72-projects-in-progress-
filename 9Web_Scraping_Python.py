e019d590bd5130496d03c5121b38f3225078472d
krishna <aryaniqoo123@gmail.com>
2026-08-20 23:05:19 +0530

    Implement web scraping for product names
    Added web scraping functionality to extract product names from Croma's mobile phones page using BeautifulSoup.

--- 9Web_Scraping_Python.py
+++ 9Web_Scraping_Python.py
@@ -1,2 +1,27 @@
+from bs4 import BeautifulSoup
 import requests
-response=requests.get("requests")
+import time
+
+r = requests.get("https://www.croma.com/phones-wearables/mobile-phones/android-phones/c/95", timeout=10)
+# print(dir(r))
+# print(r.ok)
+# print(r.status_code)
+# print("fineeee")
+# print(r.headers)
+# r = r.text
+
+soup = BeautifulSoup(r.text,"html.parser")
+# soup.find("h1")
+product = (soup.find_all(class_ = "product-item"))
+# print(product)
+# print(text.text)
+for product in product:
+    # print(heading.text)
+    # print(soup.find("div"))
+    
+    # (soup.find(class_="plp-prod-title-rating-cont"))
+    h3=(product.find("h3"))
+    na=(h3.find('a'))
+    print(na.text)
+    # print(soup.find(class_="new-price plp-srp-new-price-cont"))
+    
